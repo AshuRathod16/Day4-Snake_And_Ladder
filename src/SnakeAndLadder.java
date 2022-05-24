9df0d4d94579c148938749e5a7912e576fb18b5c
Ashwini Rathod <rashwini510@gmail.com>
2022-05-24 19:43:35 +0530

    Added:begenning of game

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,45 +1,10 @@
 import java.util.Random;
 public class SnakeAndLadder {
-    static final int noPlay = 0;
-    static final int ladder = 1;
-    static final int snake = 2;
-
-
     public static void main(String[] args) {
-        int position = 0;
+            int player1Position=0;
+            System.out.println("Game begins: player1 at position: "+player1Position);
 
-        while (position < 100) {
-            Random ran = new Random();
-            int dice = ran.nextInt(6)+1;
-
-            System.out.println("dice: " + dice);
-            int optCheck = ran.nextInt(3);
-            System.out.println("optCheck: " + optCheck);
-
-            if ((optCheck == ladder) && (position + dice) <= 100)
-            {
-                System.out.println("Ladder");
-                position = position + dice;
-            }
-
-            else if (optCheck == snake)
-            {
-                System.out.println("Snake");
-                position = position - dice;
-            }
-
-            else
-            {
-                System.out.println("No Play");
-            }
-
-            if (position < 0)
-                position = 0;
-
-            System.out.println("position: " + position);
         }
-
     }
-}
 
 
