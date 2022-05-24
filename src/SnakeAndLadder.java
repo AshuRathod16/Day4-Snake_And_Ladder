41a8a987d370e1fe5d027d0dc88aebc81ad663fd
Ashwini Rathod <rashwini510@gmail.com>
2022-05-24 11:34:19 +0530

    Added:roll a die to get random no between 1 to 6

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,5 +1,7 @@
 public class SnakeAndLadder {
     public static void main(String[] args ){
-        System.out.println("welcome");
+        int player1Position = 0;
+        int player1Dice = (int)(Math.floor(Math.random() * 10) % 6) + 1;
+      System.out.println("Dice roll value:" + player1Dice);
     }
 }
